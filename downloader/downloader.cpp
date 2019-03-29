2a6bf372985541a4c8a714c86e681c54f474ec70
vkdlsvnt <vkdlsvnt@naver.com>
2019-03-30 02:37:46 +0900

    terminal charset utf-8

--- downloader/downloader.cpp
+++ downloader/downloader.cpp
@@ -5,12 +5,35 @@ using namespace std;
 int main(void){
     
     //system("echo off");
-    cout << "<<< DEVSIGN 2019 C Homework Downloader >>>" << endl;
-    cout << "** Connecting to update server..." << endl;
+    system("chcp 65001");
+    cout << endl << endl;
+    cout << "      ___           ___           ___           ___                       ___           ___      " << endl;
+    cout << "     /\\  \\         /\\  \\         /\\__\\         /\\  \\          ___        /\\  \\         /\\__\\     " << endl;
+    cout << "    /::\\  \\       /::\\  \\       /:/  /        /::\\  \\        /\\  \\      /::\\  \\       /::|  |    " << endl;
+    cout << "   /:/\\:\\  \\     /:/\\:\\  \\     /:/  /        /:/\\ \\  \\       \\:\\  \\    /:/\\:\\  \\     /:|:|  |    " << endl;
+    cout << "  /:/  \\:\\__\\   /::\\~\\:\\  \\   /:/__/  ___   _\\:\\~\\ \\  \\      /::\\__\\  /:/  \\:\\  \\   /:/|:|  |__  " << endl;
+    cout << " /:/__/ \\:|__| /:/\\:\\ \\:\\__\\  |:|  | /\\__\\ /\\ \\:\\ \\ \\__\\  __/:/\\/__/ /:/__/_\\:\\__\\ /:/ |:| /\\__\\ " << endl;
+    cout << " \\:\\  \\ /:/  / \\:\\~\\:\\ \\/__/  |:|  |/:/  / \\:\\ \\:\\ \\/__/ /\\/:/  /    \\:\\  /\\ \\/__/ \\/__|:|/:/  / " << endl;
+    cout << "  \\:\\  /:/  /   \\:\\ \\:\\__\\    |:|__/:/  /   \\:\\ \\:\\__\\   \\::/__/      \\:\\ \\:\\__\\       |:/:/  /  " << endl;
+    cout << "   \\:\\/:/  /     \\:\\ \\/__/     \\::::/__/     \\:\\/:/  /    \\:\\__\\       \\:\\/:/  /       |::/  /   " << endl;
+    cout << "    \\::/__/       \\:\\__\\        ~~~~          \\::/  /      \\/__/        \\::/  /        /:/  /    " << endl;
+    cout << "     ~~            \\/__/                       \\/__/                     \\/__/         \\/__/     " << endl;
+ 
+    cout << endl << endl;
+
+    cout << "<<< DEVSIGN 2019 C 과제 다운로더 >>>" << endl << endl;
+
+    cout << "** 업데이트 서버에 접속하는 중입니다..." << endl;
     system("wget https://habitat.factoryal.com/devsign/2019-C/updater.exe");
+
+    cout << endl << "** 업데이터 실행 중..." << endl;
     system(".\\updater.exe");
+
+    cout << "** 업데이터 정리 중..." << endl;
     system("rm updater.exe");
-    cout << "Updater will be closed in few seconds..." << endl;
+
+    cout << "다운로더가 곧 종료됩니다..." << endl;
     Sleep(3000);
+
     return 0;
 }
